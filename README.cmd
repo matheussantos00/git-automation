9e45be1ff0e36e9d6ef6706154e23c4965491903
matheussantos00 <matheusphilippe00@gmail.com>
2020-04-23 16:56:14 -0300

    README.cmd file

---
+++ README.cmd
@@ -0,0 +1,13 @@
+# Create a local and a GItHub repository via Terminal command #
+
+1) Create a custom Terminal command to drive you to the correct directory (projetos) and create and inialize a new local repository whose name is the parameter of the terminal command.
+
+2) Web scraping GitHub's login page to enter the site
+
+
+
+# Needed #
+
+Python3.5:
+	requests
+	BeautifulSoup
