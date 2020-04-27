f6bf4f36e07b83599a7fdace7363e2cb8fed8e60
matheussantos00 <matheusphilippe00@gmail.com>
2020-04-27 16:45:36 -0300

    Execute git init and git remote with ssh key in the correct directory after run the python code

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -3,9 +3,11 @@
 function gitcr() {
   cd ~/projetos/git-automation
   python3.5 github-automation.py ${1} ${2}
+  SSH="git remote add origin git@github.com:matheussantos00/${1}.git"
   cd ~/projetos
   mkdir ${1}
   cd ${1}
-#  git init
-# return: SSH path; executar SSH path
+  git init
+  eval "${SSH}"
+  echo "first push command: git push -u origin master"
 }
