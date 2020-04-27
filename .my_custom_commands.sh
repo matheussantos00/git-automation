99dd58c736786c940c84915bbc44c75e46520888
matheussantos00 <matheusphilippe00@gmail.com>
2020-04-27 12:23:52 -0300

    Call the python script sending the name and description of the repository

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -1,8 +1,11 @@
 #!/bin/bash# prints the input
 
 function gitcr() {
+  cd ~/projetos/git-automation
+  python3.5 github-automation.py ${1} ${2}
   cd ~/projetos
   mkdir ${1}
   cd ${1}
-  git init
+#  git init
+# return: SSH path; executar SSH path
 }
