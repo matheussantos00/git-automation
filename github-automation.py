7e23df4bf1f81740445977632f99fe9945c6f201
matheussantos00 <matheusphilippe00@gmail.com>
2020-04-28 13:30:04 -0300

    Organized code

--- github-automation.py
+++ github-automation.py
@@ -1,69 +1,77 @@
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import sys
 import time
 
-name = sys.argv[1]
-description = sys.argv[2]
-#  ------------------------------------------------
-print('Inicializando repositório local e remoto: ' + name)
-try:
-    file = open('login.txt', 'x')
-    file.write(input('login: ')+'\n')
-    file.write(input('senha: '))
-    file.close()
-except:
-    pass
-finally:
-    file = open('login.txt', 'r')
-    usuario = {'login': file.readline().replace('\n', ''),
-               'password': file.readline()
-               }
-    file.close()
-#  ------------------------------------------------
 
-repositorio = {'repository[name]':name,
-                 'repository[description]':description,
-                 'repository[visibility]':'public',
-                 'repository[auto_init]':0,
-                 'repository[gitignore_template]':'',
-                 'owner': usuario['login']}
+class GitHub:
+    def __init__(self):
+        self.name = sys.argv[1]
+        self.description = sys.argv[2]
+        self.tokens = []
+        self.url = 'https://github.com'
+        print('Inicializando repositório local e remoto: ' + self.name)
 
-a = []
-#  ------------------------------------------------
-site = requests.Session()
-url = 'https://github.com'
-url1 = url + '/new'
-url2 = url + 'repositories'
+    def login_file(self):
+        try:
+            file = open('login.txt', 'x')
+            file.write(input('login: ') + '\n')
+            file.write(input('senha: '))
+            file.close()
+        except:
+            pass
+        finally:
+            file = open('login.txt', 'r')
+            self.usuario = {'login': file.readline().replace('\n', ''),
+                            'password': file.readline()}
+            file.close()
 
-login_page = site.get(url + '/login')  # request para a página de login
-time.sleep(3)
-if login_page.status_code == 200:
-    print("Request bem sucedio na página de login")
-    content = login_page.content
-    soup = BeautifulSoup(content, 'html.parser')  # ler o código da página de login e formatá-lo para HTML
-    usuario['authenticity_token'] = soup.find('input', attrs={'name': 'authenticity_token'})['value']  # token para login
-    print('Token obtido e anexado ao dicionário')
-    login = site.post(url + '/session', data=usuario)  # enviar login, senha e logar
-    if login.status_code == 200:
-        print('Login executado')
-        #  ------------------------------------------------
-        new_page = site.get(url + '/new')  # request para a página de criação de repositórios
+    def repository(self):
+        self.repo = {'repository[name]': self.name,
+                     'repository[description]': self.description,
+                     'repository[visibility]': 'public',
+                     'repository[auto_init]': 0,
+                     'repository[gitignore_template]': '',
+                     'owner': self.usuario['login']}
+
+    def scrape(self):
+        site = requests.Session()
+        login_page = site.get(self.url + '/login')  # request para a página de login
         time.sleep(3)
-        if new_page.status_code == 200:
-            print('Acesso a página de criação de repositórios')
-            content = new_page.content
-            soup = BeautifulSoup(content, 'html.parser')
-            for x in soup.find_all('input', attrs={'name': 'authenticity_token'}):  #  armazenar todos os tokens da página
-                a.append(x.attrs['value'])
 
-            repositorio['authenticity_token'] = a[2]  # token correto para criação de repositório
+        if login_page.status_code == 200:
+            print("Acesso à página de login")
+            content = login_page.content
+            soup = BeautifulSoup(content, 'html.parser')  # ler o código da página de login e formatá-lo para HTML
+            self.usuario['authenticity_token'] = soup.find('input', attrs={'name': 'authenticity_token'})['value']  # token para login
+            login = site.post(self.url + '/session', data=self.usuario)  # enviar login, senha e logar
 
-            new = site.post(url + '/repositories', data=repositorio)  # criar repositório
-            if new.status_code == 200:
-                print('Repositório criado')
-                
-    else:
-        site.close()  # fecha o request da página de login
-site.close()  # fecha o request da página de login
+            if login.status_code == 200:
+                print('Login executado')
+                new_page = site.get(self.url + '/new')  # request para a página de criação de repositórios
+                time.sleep(3)
+
+                if new_page.status_code == 200:
+                    print('Acesso à página de criação de repositórios')
+                    content = new_page.content
+                    soup = BeautifulSoup(content, 'html.parser')
+
+                    for x in soup.find_all('input', attrs={'name': 'authenticity_token'}):
+                        self.tokens.append(x.attrs['value'])  # armazenar todos os tokens da página
+
+                    self.repo['authenticity_token'] = self.tokens[2]  # token correto para criação de repositório
+                    new = site.post(self.url + '/repositories', data=self.repo)  # criar repositório
+
+                    if new.status_code == 200:
+                        print('Repositório criado')
+
+            else:
+                site.close()  # fecha o request em caso de erro
+        site.close()          # fecha o request ao fim do acesso
+
+
+if __name__ != 'main':
+    app = GitHub()
+    app.login_file()
+    app.repository()
+    app.scrape()
