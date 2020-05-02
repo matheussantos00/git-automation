c794e0d3e78fac3b23cf9a949736f9141a55b286
matheussantos00 <matheusphilippe00@gmail.com>
2020-05-02 18:57:53 -0300

    Exception handling

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -2,12 +2,16 @@
 
 function gitcr() {
   cd ~/projetos/git-automation
-  python3.5 github-automation.py ${1} "${2}"
-  SSH="git remote add origin git@github.com:matheussantos00/${1}.git"
-  cd ~/projetos
-  mkdir ${1}
-  cd ${1}
-  git init
-  eval "${SSH}"
-  echo "first push command: git push -u origin master"
+  if ! python3.5 github-automation.py ${1} "${2}"; then
+      echo "Erro, reporitório não criado!"
+      cd ~
+  else
+      SSH="git remote add origin git@github.com:matheussantos00/${1}.git"
+      cd ~/projetos
+      mkdir ${1}
+      cd ${1}
+      git init
+      eval "${SSH}"
+      echo "first push command: git push -u origin master"
+  fi
 }
